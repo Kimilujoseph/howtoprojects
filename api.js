4f51fabf8679fa5643cd8abb7217380117f0b3c1
kimilujoseph <timothyjoseph8580@gmail.com>
2023-10-05 22:45:21 +0300

    my ecommerce

--- api.js
+++ api.js
@@ -1,17 +1,18 @@
 const products = require('./productmodule')
 
 module.exports={
-    listOfproducts
+    listOfproducts,
+    productItems
 }
 
 async function listOfproducts(req,res){
     const { offset = 0 , limit=25} = req.query;
-    let data = await products.productlist({
+    let files= await products.productlist({
         offset:parseInt(req.query.offset),
         limit:parseInt(req.query.limit)
     })
     let resources = {}
-    resources.resources = data;
+    resources.resources = files;
     resources.previous={
         page:parseInt(req.query.offset) - 1,
         limit:limit
@@ -21,5 +22,13 @@ async function listOfproducts(req,res){
         limit:limit
     }
     res.json(resources)
+
+}
+
+async function productItems(req,res,next){
+    const {id} = req.params
+    let productitems= await products.filterdItem(id)
+    if(!productItems) return next()
+    res.json(productitems)
 
 }
